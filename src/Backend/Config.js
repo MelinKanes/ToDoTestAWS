93afb33450eacb19de310a593e35a0f1b932a596
MELINKOSI RUVIMBO KANENGONI <221082082@student.uj.ac.za>
2024-07-02 19:37:15 +0200

    AWS CRUD Functionality

--- src/Backend/Config.js
+++ src/Backend/Config.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 export default function configureAWS() {
+  //Just Uncomment the variables to make this work
   //const AssessKey = "AKIA2UC3FHGRHHPS4C4H";
   //const SAccessKey = "MG1T9l83pP44phUgO8/EUyjWkE5ya1dduFrqdL4v";
   AWS.config.update({
